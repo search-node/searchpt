1d676e45e60541f84e271d46718a4c6f29d39be9
Jesper Kristensen <cableman@linuxdev.dk>
2015-08-18 16:39:38 +0200

    Issue #ULF-102 by cableman: Fixed filtered search

--- js/example.search_node.config.js
+++ js/example.search_node.config.js
@@ -22,37 +22,58 @@ angular.module('searchAppConfig', [])
         'size': 8,
         'page': 0
       },
-      'cacheExpire': 30,
-      'filters': [{
-        'field': 'field_level',
-        'name': 'Level',
-        'type': 'and',
-        'terms': {
-          "1": {
-            'value': '1'
-          },
-          "2": {
-            'value': '2'
-          },
-          "3": {
-            'value': '3'
-          },
-          "4": {
-            'value': '4'
-          },
-          "5": {
-            'value': '5'
-          },
-          "6": {
-            'value': '6'
-          },
-          "7": {
-            'value': '7'
-          },
-          "8": {
-            'value': '8'
+      'cacheExpire': 5,
+      'filters': [
+        {
+          'field': 'field_level',
+          'name': 'Level',
+          'type': 'and',
+          'terms': {
+            "1": {
+              'value': '1'
+            },
+            "2": {
+              'value': '2'
+            },
+            "3": {
+              'value': '3'
+            },
+            "4": {
+              'value': '4'
+            },
+            "5": {
+              'value': '5'
+            },
+            "6": {
+              'value': '6'
+            },
+            "7": {
+              'value': '7'
+            },
+            "8": {
+              'value': '8'
+            }
+          }
+        },
+        {
+          'field': 'field_teknologi',
+          'name': 'Teknologi',
+          'type': 'and',
+          'terms': {
+            "Angular": {
+              'value': 'Angular'
+            },
+            "Apache": {
+              'value': 'Apache'
+            },
+            "Javascript": {
+              'value': 'Javascript'
+            },
+            "PHP": {
+              'value': 'PHP'
+            }
           }
         }
-      }]
+      ]
     }
   });
